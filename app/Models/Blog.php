4daf904b9d871de05eb9413cded49a6a742f87a9
git@Infosaqib <hafizsaqi555@gmail.com>
2026-06-25 12:27:29 +0500

    feat: add admin  models

---
+++ app/Models/Blog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Blog extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+
+    protected static function booted()
+    {
+        static::saving(function (Blog $blog) {
+            if ($blog->isDirty('title')) {
+                $blog->slug = Str::slug($blog->title);
+            }
+
+            if ($blog->isDirty('content')) {
+                $blog->excerpt = Str::limit(strip_tags($blog->content), 200);
+            }
+        });
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}
