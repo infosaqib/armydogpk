8f2536a674a0ff57289ba71ea606c932ce77c871
git@Infosaqib <hafizsaqi555@gmail.com>
2026-07-04 18:45:17 +0500

    fix: admin auth issues

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
-        return view('auth.login');
+        return view('admin.auth.login');
     }
 
     public function login(Request $request)
