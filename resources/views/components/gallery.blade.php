91c58b00d616288934df26437a68e76bfdca6647
git@Infosaqib <hafizsaqi555@gmail.com>
2026-07-05 10:12:18 +0500

    feat: apply responsiveness to gallery

--- resources/views/components/gallery.blade.php
+++ resources/views/components/gallery.blade.php
@@ -1,7 +1,6 @@
 <!-- Gallery Section -->
 <section class="py-20 bg-gray-50">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <!-- Header -->
         <div class="text-center mb-12">
             <h2 class="text-4xl font-bold mb-4">
                 Our <span class="text-primary">Gallery</span>
@@ -10,38 +9,40 @@
                 Explore our extensive portfolio of trained military and security dogs in action, showcasing their
                 exceptional skills, discipline, and capabilities in real-world operations.
             </p>
-
         </div>
 
-        <div class="flex flex-wrap md:-m-2 -m-1">
-            <div class="flex flex-wrap w-1/2">
-                <div class="md:p-2 p-1 w-1/2">
-                    <img alt="gallery" class="w-full object-cover h-full object-center block"
-                        src="https://dummyimage.com/500x300">
+        <div class="flex flex-wrap flex-col md:flex-row -m-1 md:-m-2">
+
+            <div class="flex flex-wrap w-full md:w-1/2">
+                <div class="p-1 md:p-2 w-full sm:w-1/2 lg:w-1/2">
+                    <img alt="gallery" class="w-full object-cover h-48 sm:h-64 lg:h-full object-center block"
+                        src="https://dummyimage.com/1200x900">
                 </div>
-                <div class="md:p-2 p-1 w-1/2">
-                    <img alt="gallery" class="w-full object-cover h-full object-center block"
-                        src="https://dummyimage.com/501x301">
+                <div class="p-1 md:p-2 w-full sm:w-1/2 lg:w-1/2">
+                    <img alt="gallery" class="w-full object-cover h-48 sm:h-64 lg:h-full object-center block"
+                        src="https://dummyimage.com/1200x900">
                 </div>
-                <div class="md:p-2 p-1 w-full">
-                    <img alt="gallery" class="w-full h-full object-cover object-center block"
-                        src="https://dummyimage.com/600x360">
-                </div>
-            </div>
-            <div class="flex flex-wrap w-1/2">
-                <div class="md:p-2 p-1 w-full">
-                    <img alt="gallery" class="w-full h-full object-cover object-center block"
-                        src="https://dummyimage.com/601x361">
-                </div>
-                <div class="md:p-2 p-1 w-1/2">
-                    <img alt="gallery" class="w-full object-cover h-full object-center block"
-                        src="https://dummyimage.com/502x302">
-                </div>
-                <div class="md:p-2 p-1 w-1/2">
-                    <img alt="gallery" class="w-full object-cover h-full object-center block"
-                        src="https://dummyimage.com/503x303">
+                <div class="p-1 md:p-2 w-full">
+                    <img alt="gallery" class="w-full object-cover h-48 sm:h-64 lg:h-full object-center block"
+                        src="https://dummyimage.com/1200x900">
                 </div>
             </div>
+
+            <div class="flex flex-wrap w-full md:w-1/2">
+                <div class="p-1 md:p-2 w-full">
+                    <img alt="gallery" class="w-full object-cover h-48 sm:h-64 lg:h-full object-center block"
+                        src="https://dummyimage.com/1200x900">
+                </div>
+                <div class="p-1 md:p-2 w-full sm:w-1/2 lg:w-1/2">
+                    <img alt="gallery" class="w-full object-cover h-48 sm:h-64 lg:h-full object-center block"
+                        src="https://dummyimage.com/1200x900">
+                </div>
+                <div class="p-1 md:p-2 w-full sm:w-1/2 lg:w-1/2">
+                    <img alt="gallery" class="w-full object-cover h-48 sm:h-64 lg:h-full object-center block"
+                        src="https://dummyimage.com/1200x900">
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
