96447ce02ebfdbfc1a97e0580e43c2536fce1dce
git@Infosaqib <hafizsaqi555@gmail.com>
2026-08-22 11:43:07 +0500

    fix: server slug based pages

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ServicePage;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function show(Request $request, $slug)
+    {
+        $page = ServicePage::where('slug', $slug)->first();
+        if ($page) {
+            return view('services.view', [
+                'page' => $page,
+            ]);
+        }
+        abort(404);
+    }
 }
